a7eb126187a083db03ec702797189330050007c6
Lucas Moncada <lucas.moncada08@gmail.com>
2020-11-01 16:10:00 -0500

    Added Timetable Search Functionality When Given Subject and Course Code

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.get('/api/subjects&description', (req, res) => {
     res.send(subjAndDescr);
 });
 
-
 app.get('/api/coursecodes/:code', (req, res) => {
     console.log(`Get request for ${req.url}`);
     var courseCodes = [];
@@ -25,6 +24,40 @@ app.get('/api/coursecodes/:code', (req, res) => {
         res.send(courseCodes)
     else
         res.status(404).send("The course ID given does not exist");
+});
+
+app.get('/api/timetable/:subjCode/:courseCode', (req, res) => {
+    console.log(`Get request for ${req.url}`);
+    var times = [];
+    for (var i=0; i<json.length; i++) {
+        if (json[i]["subject"] === req.params.subjCode && json[i]["catalog_nbr"] === req.params.courseCode) {
+            times = times.concat(json[i]["course_info"][0]["start_time"]);
+            times = times.concat(json[i]["course_info"][0]["end_time"]);
+            times = times.concat(json[i]["course_info"][0]["ssr_component"]);
+        }
+    }
+    if (times.length > 0)
+        res.send(times);
+    else
+        res.status(404).send("The subject code and course code combination is not valid");
+});
+
+app.get('/api/timetable/:subjCode/:courseCode/:component', (req, res) => {
+    console.log(`Get request for ${req.url}`);
+    
+});
+
+// Function for outputting the few items that are not lecture components
+app.get('/api/coursecomponents', (req, res) => {
+    console.log(`Get request for ${req.url}`);
+    var count = 0;
+    for (var i=0; i<json.length; i++) {
+        if (json[i]["course_info"][0]["ssr_component"] != 'LEC') {
+            console.log(json[i]);
+            count++;
+        }
+    }
+    res.send(count.toString());
 });
 
 const port = process.env.PORT || 3000;
