e13be25295f65026c4fdc8d5a0048163b5f518e5
Lucas Moncada <lucas.moncada08@gmail.com>
2020-11-01 16:16:38 -0500

    Added Optional Course Component Searching Included with the Subject and Course Code

--- index.js
+++ index.js
@@ -44,7 +44,17 @@ app.get('/api/timetable/:subjCode/:courseCode', (req, res) => {
 
 app.get('/api/timetable/:subjCode/:courseCode/:component', (req, res) => {
     console.log(`Get request for ${req.url}`);
-    
+    var times = [];
+    for (var i=0; i<json.length; i++) {
+        if (json[i]["subject"] === req.params.subjCode && json[i]["catalog_nbr"] === req.params.courseCode && json[i]["course_info"][0]["ssr_component"] === req.params.component) {
+            times = times.concat(json[i]["course_info"][0]["start_time"]);
+            times = times.concat(json[i]["course_info"][0]["end_time"]);
+        }
+    }
+    if (times.length > 0)
+        res.send(times);
+    else
+        res.status(404).send("The subject code and course code combination is not valid");
 });
 
 // Function for outputting the few items that are not lecture components
