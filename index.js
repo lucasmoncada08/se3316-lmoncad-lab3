08f6dcd03899a304bfa0c409204393bc59c9eaba
Lucas Moncada <lucas.moncada08@gmail.com>
2020-11-01 15:31:47 -0500

    Added Course Code Functionality

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 let json = require('./Lab3-timetable-data.json');
 
-app.get('/api/courses', (req, res) => {
+app.get('/api/subjects&description', (req, res) => {
     console.log(`Get request for ${req.url}`);
     var subjAndDescr = [];
     for (var i=0; i<json.length; i++) {
@@ -11,6 +11,17 @@ app.get('/api/courses', (req, res) => {
         subjAndDescr = subjAndDescr.concat(json[i]["className"]);
     }
     res.send(subjAndDescr);
+});
+
+
+app.get('/api/coursecodes/:code', (req, res) => {
+    console.log(`Get request for ${req.url}`);
+    var courseCodes = [];
+    for (var i=0; i<json.length; i++) {
+        if (json[i]["subject"] === req.params.code)
+            courseCodes = courseCodes.concat(json[i]["catalog_nbr"]);
+    }
+    res.send(courseCodes)
 });
 
 const port = process.env.PORT || 3000;
